17411604ef163b38e4a887df831469ef87a91db3
Luis Lozano <lslozano11@gmail.com>
2024-08-30 17:25:10 -0600

    Remove unnecessary code

--- tests/comment/validateSave.test.js
+++ tests/comment/validateSave.test.js
@@ -3,19 +3,6 @@ const { CommentSchema } = require("../../src/api/models/Comment");
 
 const Comment = mongoose.model("Comment", CommentSchema);
 
-beforeAll(async () => {
-  const uri = "mongodb://localhost:27017/task-manager-api";
-  await mongoose.connect(uri);
-});
-
-afterAll(async () => {
-  await mongoose.disconnect();
-});
-
-afterEach(async () => {
-  await Comment.deleteMany({});
-});
-
 describe("Comment Save", () => {
   it("should save the Comment when all required fields are provided", async () => {
     const userId = new mongoose.Types.ObjectId();
