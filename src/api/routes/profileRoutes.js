a84a77735506c60223279a04b88ae38edaccec86
Luis Lozano <lslozano11@gmail.com>
2024-09-22 19:50:50 -0600

    Add profile routes with controllers and middlewares

--- src/api/routes/profileRoutes.js
+++ src/api/routes/profileRoutes.js
@@ -1,8 +1,22 @@
 const express = require("express");
-const { viewProfile } = require("../controllers/profileController.js");
+const { logoutUser, deleteUser } = require("../controllers/userController.js");
+const {
+  viewProfile,
+  editProfile,
+  viewAssignedTasks,
+  viewPublishedComments,
+} = require("../controllers/profileController.js");
 
 const profileRouter = express.Router();
 
+// All available actions at Profile
 profileRouter.get("/", viewProfile);
+profileRouter.patch("/edit", editProfile);
+profileRouter.get("/tasks", viewAssignedTasks);
+profileRouter.get("/comments", viewPublishedComments);
+
+// Calls User controllers to logout or delete.
+profileRouter.post("/logout", logoutUser);
+profileRouter.delete("/delete", deleteUser);
 
 module.exports = profileRouter;
